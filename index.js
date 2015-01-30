5f97f879e42f5eaa6ef52cfe11b938f8eb26e08e
s.ksenofontov <s.ksenofontov@sup.com>
2015-01-30 14:00:26 +0300

    some structures implemented.

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-const db = require('./db/index.js');
+const db = require('./server/db/index.js');
+const koa = require('koa');
+
+const app = koa();
+
 
 console.log('db successefully created!');
+
+app.use(function *(next) {
+  var start = new Date();
+
+  yield next;
+
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(require('koa-static')('./client'));
+
+app.use(function *articlesGetter(next) {
+
+  this.state.posts = yield db.Article.findAll({
+    limit: 3,
+    order: [['id', 'DESC']],
+  });
+
+  yield next;
+
+});
+
+app.use(function *() {
+  var self = this;
+
+  this.body = '';
+
+  if (this.state.posts) {
+    this.state.posts.forEach(function(v) {
+      self.body += 'id: ' + v.dataValues.id + ', header: ' + v.dataValues.header + ', text: ' + v.dataValues.text + '\n';
+    });
+  }
+
+  else
+    this.body += 'Nothing to show!'
+});
+
+app.on('error', function(err, ctx) {
+  console.log('Error is occured: ', err);
+})
+
+app.listen(3000);
