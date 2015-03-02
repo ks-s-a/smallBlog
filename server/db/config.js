93548f43e81c3eaa3274758de92c62f4edcd8204
ks-s-a <ksenofontovsa@ya.ru>
2015-03-02 15:04:12 +0300

    try13

--- server/db/config.js
+++ server/db/config.js
@@ -4,7 +4,7 @@ var match = process.env.DATABASE_URL.match(/^postgres:\/\/([^:]+):([^@]+)@([^:]+
 console.log('match[0] is: ' + match[0], 'match[1] is: ' + match[1],
   'match[2] is: ' + match[2], 'match[3] is: ' + match[3], 'match[4] is: ' + match[4], 'match[5] is: ' + match[5]);
 module.exports = {
-  database: match[0],
+  database: match[5],
   username: match[1],
   password: match[2],
   dialect: 'postgres',
