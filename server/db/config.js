d30726943babe9e0f3c71eb0464fce2676ca6ce6
ks-s-a <ksenofontovsa@ya.ru>
2015-03-02 13:38:48 +0300

    try6

--- server/db/config.js
+++ server/db/config.js
@@ -1,5 +1,5 @@
 console.log('process.env.DATABASE_URL is: ', process.env.DATABASE_URL);
-var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):([^\/]+)\/(.+)/);
 
 module.exports = {
   database: match[5],
